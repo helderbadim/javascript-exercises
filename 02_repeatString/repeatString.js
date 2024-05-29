eaaa58f98c99ff54ca3c9f56d303b62be6b37990
Helder Badim <helderbadim@hotmail.com>
2024-05-29 14:54:38 +0100

    exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+  let finalString = ``;
+  for (let i = 1; i <= number; i++) {
+    finalString += string;
+  }
+  return finalString;
 };
+
+repeatString(`hey`, 3);
 
 // Do not edit below this line
 module.exports = repeatString;
