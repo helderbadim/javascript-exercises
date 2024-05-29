d857280370a647e4c4978f6ac6312ac4ee974397
Helder Badim <helderbadim@hotmail.com>
2024-05-30 00:32:01 +0100

    exercise 3 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (originalString) {
+  let reverse = ``;
+  for (let i = originalString.length - 1; i >= 0; i--) {
+    reverse += originalString[i];
+  }
+  return reverse;
 };
+
+reverseString(`hello`);
 
 // Do not edit below this line
 module.exports = reverseString;
