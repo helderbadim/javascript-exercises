43652917921e5bb42888ff9acdb6ebde19fc8b7e
Helder Badim <helderbadim@hotmail.com>
2024-05-30 00:32:59 +0100

    exercise 4 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...elementToDelete) {
+  const modifiedArray = [];
+  array.forEach((element) => {
+    if (!elementToDelete.includes(element)) {
+      modifiedArray.push(element);
+    }
+  });
+  return modifiedArray;
 };
+
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
