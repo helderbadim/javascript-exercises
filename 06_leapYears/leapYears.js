90d63565d2bee15d591c21d45de5c646fb7dffd2
Helder Badim <helderbadim@hotmail.com>
2024-05-30 18:17:31 +0100

    exercise 6 completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  let remainderOfFour = year % 4;
+  let remainderOf100 = year % 100;
+  let remainderOf400 = year % 400;
+  if (remainderOfFour === 0 && remainderOf100 === 0 && remainderOf400 === 0) {
+    return true;
+  } else if (remainderOfFour === 0 && remainderOf100 !== 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
+leapYears(2000);
 
 // Do not edit below this line
 module.exports = leapYears;
