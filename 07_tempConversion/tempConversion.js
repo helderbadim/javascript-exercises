e4cf73eaceb927913bdc3a9aed4a159ffa42ea7e
Helder Badim <helderbadim@hotmail.com>
2024-05-30 18:18:14 +0100

    exercise 7 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempFahrenheit) {
+  let tempCelsius = Number((((tempFahrenheit - 32) * 5) / 9).toFixed(1));
+  return tempCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempCelsius) {
+  let tempFahrenheit = Number(((tempCelsius * 9) / 5 + 32).toFixed(1));
+  return tempFahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
